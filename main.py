c2aab953a157c6dcb77b5dbc237474f4d4df1170
CsanadiBence <127303953+CsanadiBence@users.noreply.github.com>
2024-04-12 08:35:56 +0200

    kk

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from pyscript import document
     
+def szamit(event):
+    szam1 = float(document.querySelector("#szam1").value)
+    szam2 = float(document.querySelector("#szam2").value)
+    muvelet = document.querySelector("#muvelet").value
+    document.querySelector("#eredmeny").textContent =""
+    
+    
+def torol(event):
+    document.querySelector("#szam1").value = ""
+    document.querySelector("#szam2").value = ""
+    document.querySelector("#eredmeny").textContent = ""
+    document.querySelector("#muvelet").selectedIndex = 0
